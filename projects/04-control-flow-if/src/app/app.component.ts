31981a27c8ed98943cd8993aebc2dd7aa35f32e9
cfurley <caleb.furley@gmail.com>
2026-03-19 21:45:31 +0000

    added accounts userinfo to to the credit card in the rendered html output.

--- projects/04-control-flow-if/src/app/app.component.ts
+++ projects/04-control-flow-if/src/app/app.component.ts
@@ -7,19 +7,21 @@ import { AccountInfo } from './account-info';
   template: `
     <article class="card">
       <h1 class="card-title">Frontend Masters Express</h1>
-      <p class="card-number">0000 0000 0000 0000</p>
+      <p class="card-number">{{ account.card }}</p>
       <section class="membership-info">
         <p>
-          <!-- name -->
-          {{ '' }}
+          {{ account.name }}
         </p>
-        <p>Valid Thru: {{ '' }}</p>
-        <p>CVV: {{ '' }}</p>
+        <p>Valid Thru: {{ account.exp }}</p>
+        <p>CVV: {{ account.cvv }}</p>
         <p>
-          <!-- membership status -->
-          <span class="badge gold">Gold</span>
-          <span class="badge platinum">Platinum</span>
-          <span class="badge silver">Silver</span>
+          @if (account.membership == 'gold') {
+            <span class="badge gold">Gold</span>
+          } @else if (account.membership == 'platinum') {
+            <span class="badge platinum">Platinum</span>
+          } @else {
+            <span class="badge silver">Silver</span>
+          }
         </p>
       </section>
     </article>
