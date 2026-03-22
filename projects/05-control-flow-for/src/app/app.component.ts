59a507c519861ce18ecb99b55e7ae3d174d1076b
cfurley <caleb.furley@gmail.com>
2026-03-22 17:05:08 +0000

    implement project 5.

--- projects/05-control-flow-for/src/app/app.component.ts
+++ projects/05-control-flow-for/src/app/app.component.ts
@@ -6,31 +6,37 @@ import { Component } from '@angular/core';
   template: `
     <section class="container">
       <!-- This article element represents and entire listing -->
-      <article class="listing">
-        <div class="image-parent">
-          <img class="product-image" src="https://placehold.co/100x100" />
-        </div>
-        <section class="details">
-          <p class="title"><!-- car make and model--></p>
-          <hr />
-          <p class="detail">
-            <span>Year</span>
-            <span><!-- year --></span>
-          </p>
-          <div class="detail">
-            <span>Transmission</span>
-            <span><!-- transmission --></span>
+      @for (car of carList; track car) {
+        <article class="listing">
+          <div class="image-parent">
+            @if (car.image) {
+              <img class="product-image" [src]="car.image" />
+            } @else {
+              <img class="product-image" src="https://placehold.co/100x100" />
+            }
           </div>
-          <p class="detail">
-            <span>Mileage</span>
-            <span><!-- miles --></span>
-          </p>
-          <p class="detail">
-            <span>Price</span>
-            <span><!-- price --></span>
-          </p>
-        </section>
-      </article>
+          <section class="details">
+            <p class="title">{{ car.make }} {{ car.model }}</p>
+            <hr />
+            <p class="detail">
+              <span>Year</span>
+              <span>{{ car.year }}</span>
+            </p>
+            <div class="detail">
+              <span>Transmission</span>
+              <span>{{ car.transmission }}</span>
+            </div>
+            <p class="detail">
+              <span>Mileage</span>
+              <span>{{ car.miles }}</span>
+            </p>
+            <p class="detail">
+              <span>Price</span>
+              <span>{{ car.price }}</span>
+            </p>
+          </section>
+        </article>
+      }
     </section>
   `,
   styleUrl: 'app.component.css',
@@ -38,32 +44,34 @@ import { Component } from '@angular/core';
 export class AppComponent {
   carList = [
     {
-      make: 'Foyoda',
-      model: 'Famery',
+      image:
+        'https://mystrongad.com/TOA_ToyotaofArdmore/TOA_Interactive/Tacoma/2022/22%20Tacoma/22-Toyota-Tacoma-Limited-Black-LG.png',
+      make: 'Toyota',
+      model: 'Tacoma',
       miles: 54354,
-      price: 1000,
+      price: 21000,
       year: 2022,
       transmission: 'Automatic',
     },
     {
-      make: 'Ronda',
-      model: 'Disaccord',
-      miles: 100000,
-      price: 230,
+      make: 'Honda',
+      model: 'Accord',
+      miles: 300000,
+      price: 2000,
       year: 1991,
       transmission: 'Automatic',
     },
     {
-      make: 'Specific Motors',
-      model: 'Spoke',
-      miles: 100000,
-      price: 230,
-      year: 1991,
+      make: 'Dodge',
+      model: 'Ram 1500',
+      miles: 200000,
+      price: 14000,
+      year: 2008,
       transmission: 'Automatic',
     },
     {
-      make: 'Fjord',
-      model: 'Pocus',
+      make: 'Ford',
+      model: 'Focus',
       miles: 1,
       price: 22330,
       year: 2023,
